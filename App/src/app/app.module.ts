fa2baadbfa527bc5f58aa60dd5eeb31375cfe1fc
Mohammed Alaa <moalaacs@gmail.com>
2023-03-22 02:28:22 +0200

    Solve conflict

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
