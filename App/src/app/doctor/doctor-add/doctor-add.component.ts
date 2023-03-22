7e47b51e36413e9ba42b1824a03aff1f05168db7
Amr Allam <amr.i.allam@hotmail.com>
2023-03-22 22:21:00 +0200

    doctor

--- App/src/app/doctor/doctor-add/doctor-add.component.ts
+++ App/src/app/doctor/doctor-add/doctor-add.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute,Router } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
-
-import { Doctor } from 'e:/ITI/Technical Fundamentals/Angular/Project/Clinic-Management-System-MEAN-Stack/App/src/app/models/doctor';
-import { DoctorService } from 'e:/ITI/Technical Fundamentals/Angular/Project/Clinic-Management-System-MEAN-Stack/App/src/app/services/doctor.service';
+import { Doctor } from 'src/app/models/doctor';
+import { DoctorService } from 'src/app/services/doctor.service';
 @Component({
   selector: 'app-doctor-add',
   templateUrl: './doctor-add.component.html',
@@ -74,7 +72,7 @@ export class DoctorAddComponent implements OnInit {
       pattern: 'Password should be a string',
       minlength: 'Length of password should be greater than 7 characters'
     },
-        clinicId: {
+    clinicId: {
       required: 'Clinic Id is required.',
       pattern: 'Clinic Id should be a number'
     },
@@ -96,101 +94,101 @@ export class DoctorAddComponent implements OnInit {
     private router: Router,
     private doctorService: DoctorService,
     private snackBar: MatSnackBar,
-    ) {
+  ) {
 
-      this.minDate = new Date('1963-01-01');
-      this.maxDate = new Date('2000-12-31');
-      this.defaultDate = new Date('1999-01-10');
+    this.minDate = new Date('1963-01-01');
+    this.maxDate = new Date('2000-12-31');
+    this.defaultDate = new Date('1999-01-10');
 
-      this.doctorForm = this.fb.group({
-        firstname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
-        lastname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
-        dateOfBirth: ['', [Validators.required, Validators.pattern('^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}$')]],
-        gender: [''],
-        phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]*')]],
-        email: ['', [Validators.required, Validators.email]],
-        address: this.fb.group({
-          street: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2)]],
-          city: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
-          country: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2)]],
-          zipCode: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(5)]]
+    this.doctorForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
+      lastname: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
+      dateOfBirth: ['', [Validators.required, Validators.pattern('^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[0-9]{4}$')]],
+      gender: [''],
+      phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      email: ['', [Validators.required, Validators.email]],
+      address: this.fb.group({
+        street: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2)]],
+        city: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]],
+        country: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2)]],
+        zipCode: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(5)]]
+      }),
+      password: ['', [
+        Validators.required,
+        Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=[\\]{};\'\\:"|,.<>\\/?]).{7,}$'),
+        Validators.minLength(8)
+      ]],
+      image: [''],
+      medicalHistory: '',
+      invoices: [],
+
+      schedule: this.fb.group({
+        monday: this.fb.group({
+          start: [null],
+          end: [null]
         }),
-        password: ['', [
-          Validators.required,
-          Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=[\\]{};\'\\:"|,.<>\\/?]).{7,}$'),
-          Validators.minLength(8)
-        ]],
-              image: [''],
-        medicalHistory: '',
-        invoices: [],
-
-        schedule: this.fb.group({
-          monday: this.fb.group({
-            start: [null],
-            end: [null]
-          }),
-          tuesday: this.fb.group({
-            start: [null],
-            end: [null]
-          }),
-          wednesday: this.fb.group({
-            start: [null],
-            end: [null]
-          }),
-          thursday: this.fb.group({
-            start: [null],
-            end: [null]
-          }),
-          friday: this.fb.group({
-            start: [null],
-            end: [null]
-          }),
-          saturday: this.fb.group({
-            start: [null],
-            end: [null]
-          }),
-          sunday: this.fb.group({
-            start: [null],
-            end: [null]
-          })
+        tuesday: this.fb.group({
+          start: [null],
+          end: [null]
         }),
-        clinicId: ['', [Validators.required, Validators.pattern('[0-9]*')]],
-        speciality: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]]
-      });
+        wednesday: this.fb.group({
+          start: [null],
+          end: [null]
+        }),
+        thursday: this.fb.group({
+          start: [null],
+          end: [null]
+        }),
+        friday: this.fb.group({
+          start: [null],
+          end: [null]
+        }),
+        saturday: this.fb.group({
+          start: [null],
+          end: [null]
+        }),
+        sunday: this.fb.group({
+          start: [null],
+          end: [null]
+        })
+      }),
+      clinicId: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      speciality: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(3)]]
+    });
 
 
-    }
+  }
 
   ngOnInit(): void {
 
-      }
+  }
 
 
-      onSubmit() {
-        const date = new Date(this.doctorForm.value.dateOfBirth);
-        const day = date.getDate().toString().padStart(2, '0');
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const year = date.getFullYear().toString();
-        const formattedDate = `${day}/${month}/${year}`;
-        this.doctorForm.value.dateOfBirth = formattedDate;
+  onSubmit() {
+    const date = new Date(this.doctorForm.value.dateOfBirth);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    const formattedDate = `${day}/${month}/${year}`;
+    this.doctorForm.value.dateOfBirth = formattedDate;
 
-        const doctor = this.doctorForm.value;
-        this.doctorService.addDoctor(doctor).subscribe(
-          () => this.router.navigate(['/doctor']))
-      }
+    const doctor = this.doctorForm.value;
+    this.doctorService.addDoctor(doctor).subscribe(
+      () => this.router.navigate(['/doctor']))
+  }
 
-      onFileSelected(event: any) {
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          this.doctorForm.patchValue({
-            image: reader.result as string
-          });
-        };
-      }
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.doctorForm.patchValue({
+        image: reader.result as string
+      });
+    };
+  }
 
-      goBack(): void {
-        this.router.navigate(['/doctor']);
-      }
-    }
+  goBack(): void {
+    this.router.navigate(['/doctor']);
+  }
+}
