d7b173675604a07f304bcf3f04fd65bef52d4ad9
Ahmed Hassan <eng.hsn10@gmail.com>
2023-03-24 21:28:40 +0200

    medicine

--- App/src/app/medicine/med-list/med-list.component.ts
+++ App/src/app/medicine/med-list/med-list.component.ts
@@ -14,6 +14,17 @@ export class MedListComponent {
   constructor(public medicineService: MedicineService, public router: Router, public location: Location) {
 
   }
+  delete(id: number) {
+    if (confirm('Are you sure you want to delete this medicine?!')) {
+      this.medicineService.deleteMedicineById(id).subscribe(a => {
+        // console.log(a);
+        this.medicineService.getAllMedicine().subscribe(() => {
+        })
+        // this.location.back();
+      })
+    }
+    this.router.navigateByUrl("/medicine");
+  }
   ngOnInit() {
     this.medicineService.getAllMedicine().subscribe(data => {
       this.medicine = data;
